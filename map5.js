db99c419ba3c097c5d4b789807eb570d90995eed
NicCapule <78493066+NicCapule@users.noreply.github.com>
2024-05-09 14:33:36 +0800

    Revert "Merge branch 'main' of https://github.com/NicCapule/MAPua-Soft-Eng"
    This reverts commit 57a59c9d2d2f880d7cf1c5871eaa7f30f79da89d, reversing
    changes made to f6a223214d386fca3cfac137eeaacc1c92858acb.

--- map5.js
+++ map5.js
@@ -53,7 +53,7 @@ class Graph5 {
           const path = queue.shift();
           const currentVertex = path[path.length - 1]; //reduces the path.length by one for every node passed
 
-          if (visited.has(currentVertex.id)) {
+          if (visited.has(currentVertex.id)) { 
               continue;
           }
 
@@ -78,38 +78,38 @@ class Graph5 {
 
 
 
-
+  
   function findShortestPath5() {
 	if (document.getElementById('initialFloorBookmark').value != 5) //change according to floor number
 		startVertex = 'Elevator Lobby'
 	else startVertex = document.getElementById('initialLocationBookmark').value;
 
-
+    
 	if (document.getElementById('desiredFloorBookmark').value != 5) //change according to floor number
 		endVertex = 'Elevator Lobby'
 	else endVertex = document.getElementById('desiredLocationBookmark').value;
 
-
-
-
+	
+	
+    
     var backgroundImage5 = new Image();
-    backgroundImage5.src = "images/pic5.png";
+    backgroundImage5.src = "images/pic5.png"; 
 
     //NODES COORDINATES
     const graph5 = new Graph5();
-
+	
 	//South Rooms
-
-  graph5.addVertex('Photography Studio 511', 854, 427)
+	
+  graph5.addVertex('Photography Studio 511', 854, 1022)
   graph5.addVertex('Control Room', 755, 493)
   graph5.addVertex('Radio Production Studio 510', 807, 456)
   graph5.addVertex('Sound Lock', 755, 456)
-  graph5.addVertex('TV Production Studio 509', 617, 427) //509 Lecture Room
-  graph5.addVertex('News Room 508', 490, 427)	//508
-  graph5.addVertex('Magic Lab 507', 452, 427) //507
-  graph5.addVertex('Lecture Room 506', 265, 427) //506
-  graph5.addVertex('Lecture Room 505', 162, 427) //505
-
+  graph5.addVertex('TV Production Studio 509', 617, 1022) //509 Lecture Room
+  graph5.addVertex('News Room 508', 490, 1022)	//508
+  graph5.addVertex('Magic Lab 507', 452, 1022) //507
+  graph5.addVertex('Lecture Room 506', 265, 1022) //506
+  graph5.addVertex('Lecture Room 505', 162, 1022) //505
+  
 	//South Rooms out
  graph5.addVertex('Photograph5y Studio 511_Out', 854, 326)
   graph5.addVertex('Sound Lock_Out', 755, 326)
@@ -118,7 +118,7 @@ class Graph5 {
   graph5.addVertex('Magic Lab 507_Out', 452, 326) //507
   graph5.addVertex('Lecture Room 506_Out', 265, 326) //506
   graph5.addVertex('Lecture Room 505_Out', 162, 326) //505
-
+  
 
 	//North Rooms
   graph5.addVertex('Elevator Lobby', 370, 165)
@@ -133,63 +133,63 @@ class Graph5 {
   graph5.addVertex('Magic lab 501_Out', 162, 326)
   graph5.addVertex('Art Studio 502_Out', 228, 326)
   graph5.addVertex('Bathroom_Out', 559, 326)
-   graph5.addVertex('Magic lab 503_Out', 723, 326)
-  graph5.addVertex('Lecture Room 504_Out', 813, 326)
-  graph5.addVertex('SMS Faculty_Out', 935, 326)
+   graph5.addVertex('Magic lab 503_Out', 723, 326) 
+  graph5.addVertex('Lecture Room 504_Out', 813, 326) 
+  graph5.addVertex('SMS Faculty_Out', 935, 326) 
+  
+  
+  
 
-
-
-
-
-
+  
+  
   //DRAWS LINES FROM A NODE TO THEIR NEAREST NEIGHBOURING NODES
-  graph5.addEdge('Elevator Lobby', 'Elevator Lobby_Out');
-  graph5.addEdge('Magic Lab 501', 'Magic Lab 501_Out');
-  graph5.addEdge('Art Studio 502', 'Art Studio 502_Out');
-  graph5.addEdge('Bathroom', 'Bathroom_Out');
-  graph5.addEdge('Magic Lab 503', 'Magic Lab 503_Out');
-  graph5.addEdge('Lecture Room 504', 'Lecture Room 504_Out');
-  graph5.addEdge('SMS Faculty', 'SMS Faculty_Out');
-
+  graph5.addEdge('Elevator Lobby', 'Elevator Lobby_Out'); 
+  graph5.addEdge('Magic Lab 501', 'Magic Lab 501_Out'); 
+  graph5.addEdge('Art Studio 502', 'Art Studio 502_Out'); 
+  graph5.addEdge('Bathroom', 'Bathroom_Out'); 
+  graph5.addEdge('Magic Lab 503', 'Magic Lab 503_Out'); 
+  graph5.addEdge('Lecture Room 504', 'Lecture Room 504_Out'); 
+  graph5.addEdge('SMS Faculty', 'SMS Faculty_Out'); 
+  
   graph5.addEdge('Photograph5y Studio 511', 'Photograph5y Studio 511_Out');
   graph5.addEdge('Sound Lock', 'Sound Lock_Out');
   graph5.addEdge('TV Production Studio 509', 'TV Production Studio 509_Out');
   graph5.addEdge('News Room 508', 'News Room 508_Out');
-  graph5.addEdge('Magic Lab 507', 'Magic Lab 507_Out');
-  graph5.addEdge('Lecture Room 506', 'Lecture Room 506_Out');
-  graph5.addEdge('Lecture Room 505', 'Lecture Room 505_Out');
-
-  graph5.addEdge('Bathroom_Out', 'Elevator Lobby_Out');
-  graph5.addEdge('Art Studio 502_Out', 'Elevator Lobby_Out');
+  graph5.addEdge('Magic Lab 507', 'Magic Lab 507_Out'); 
+  graph5.addEdge('Lecture Room 506', 'Lecture Room 506_Out'); 
+  graph5.addEdge('Lecture Room 505', 'Lecture Room 505_Out'); 
+  
+  graph5.addEdge('Bathroom_Out', 'Elevator Lobby_Out'); 
+  graph5.addEdge('Art Studio 502_Out', 'Elevator Lobby_Out'); 
   graph5.addEdge('News Room 508_Out', 'Elevator Lobby_Out');
-  graph5.addEdge('Magic Lab 507_Out', 'Elevator Lobby_Out');
-  graph5.addEdge('Lecture Room 506_Out', 'Elevator Lobby_Out');
-  graph5.addEdge('Sound Lock', 'Radio Production Studio 510');
-  graph5.addEdge('Sound Lock', 'Control Room');
-  graph5.addEdge('Magic Lab 507_Out', 'News Room 508_Out');
-  graph5.addEdge('TV Production Studio 509_Out', 'News Room 508_Out');
-  graph5.addEdge('TV Production Studio 509_Out', 'Sound Lock_Out');
-  graph5.addEdge('Photograph5y Studio 511_Out', 'Sound Lock_Out');
-  graph5.addEdge('Photograph5y Studio 511_Out', 'Magic Lab 503_Out');
-  graph5.addEdge('Photograph5y Studio 511_Out', 'SMS Faculty_Out');
-  graph5.addEdge('Lecture Room 504', 'SMS Faculty_Out');
-  graph5.addEdge('Photograph5y Studio 511_Out', 'Lecture Room 504_Out');
-  graph5.addEdge('Magic Lab 503_Out', 'Lecture Room 504_Out');
-  graph5.addEdge('Magic Lab 503_Out', 'TV Production Studio 509_Out');
-  graph5.addEdge('Magic Lab 503_Out', 'Sound Lock_Out');
-  graph5.addEdge('Lecture Room 504_Out', 'Sound Lock_Out');
-  graph5.addEdge('Magic Lab 503_Out', 'Bathroom_Out');
-  graph5.addEdge('TV Production Studio 509_Out', 'Bathroom_Out');
-  graph5.addEdge('News Room 508_Out', 'Bathroom_Out');
-  graph5.addEdge('Magic Lab 507_Out', 'Bathroom_Out');
-  graph5.addEdge('Sound Lock_Out', 'Bathroom_Out');
-  graph5.addEdge('Magic Lab 507_Out', 'Lecture Room 506_Out');
-  graph5.addEdge('Magic Lab 507_Out', 'Art Studio 502_Out');
-  graph5.addEdge('Magic Lab 505_Out', 'Lecture Room 506_Out');
-  graph5.addEdge('Art Studio 502_Out', 'Lecture Room 506_Out');
-  graph5.addEdge('Art Studio 502_Out', 'Magic lab 501_Out');
-  graph5.addEdge('Art Studio 502_Out', 'Lecture Room 505_Out');
-  graph5.addEdge('Lecture Room 505_Out', 'Magic lab 501_Out');
+  graph5.addEdge('Magic Lab 507_Out', 'Elevator Lobby_Out'); 
+  graph5.addEdge('Lecture Room 506_Out', 'Elevator Lobby_Out'); 
+  graph5.addEdge('Sound Lock', 'Radio Production Studio 510'); 
+  graph5.addEdge('Sound Lock', 'Control Room'); 
+  graph5.addEdge('Magic Lab 507_Out', 'News Room 508_Out'); 
+  graph5.addEdge('TV Production Studio 509_Out', 'News Room 508_Out'); 
+  graph5.addEdge('TV Production Studio 509_Out', 'Sound Lock_Out'); 
+  graph5.addEdge('Photograph5y Studio 511_Out', 'Sound Lock_Out'); 
+  graph5.addEdge('Photograph5y Studio 511_Out', 'Magic Lab 503_Out'); 
+  graph5.addEdge('Photograph5y Studio 511_Out', 'SMS Faculty_Out'); 
+  graph5.addEdge('Lecture Room 504', 'SMS Faculty_Out'); 
+  graph5.addEdge('Photograph5y Studio 511_Out', 'Lecture Room 504_Out'); 
+  graph5.addEdge('Magic Lab 503_Out', 'Lecture Room 504_Out'); 
+  graph5.addEdge('Magic Lab 503_Out', 'TV Production Studio 509_Out'); 
+  graph5.addEdge('Magic Lab 503_Out', 'Sound Lock_Out'); 
+  graph5.addEdge('Lecture Room 504_Out', 'Sound Lock_Out'); 
+  graph5.addEdge('Magic Lab 503_Out', 'Bathroom_Out'); 
+  graph5.addEdge('TV Production Studio 509_Out', 'Bathroom_Out'); 
+  graph5.addEdge('News Room 508_Out', 'Bathroom_Out'); 
+  graph5.addEdge('Magic Lab 507_Out', 'Bathroom_Out'); 
+  graph5.addEdge('Sound Lock_Out', 'Bathroom_Out'); 
+  graph5.addEdge('Magic Lab 507_Out', 'Lecture Room 506_Out'); 
+  graph5.addEdge('Magic Lab 507_Out', 'Art Studio 502_Out'); 
+  graph5.addEdge('Magic Lab 505_Out', 'Lecture Room 506_Out'); 
+  graph5.addEdge('Art Studio 502_Out', 'Lecture Room 506_Out'); 
+  graph5.addEdge('Art Studio 502_Out', 'Magic lab 501_Out'); 
+  graph5.addEdge('Art Studio 502_Out', 'Lecture Room 505_Out'); 
+  graph5.addEdge('Lecture Room 505_Out', 'Magic lab 501_Out'); 
 
   const shortestPath5 = graph5.bfs(startVertex, endVertex);
   var canvas5 = document.getElementById('canvas5');
@@ -197,8 +197,8 @@ class Graph5 {
 
   // Clear the canvas
   //ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-
+  
+  
   if (shortestPath5) {
     console.log(`Shortest path from ${startVertex} to ${endVertex}: ${shortestPath5.join(' -> ')}`);
 
@@ -226,3 +226,7 @@ class Graph5 {
 }
 
   }
+
+  
+  
+  
